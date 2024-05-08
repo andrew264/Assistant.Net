ac440083d1cea2a54e2d532edc86421eb0c8fa88
Andrew Santhosh <andrewsanthosh264@gmail.com>
2024-05-08 06:41:24 +0530

    initial commit;

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using Assistant.Net.Utils;
+using Discord;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Assistant.Net
+{
+    public class Program
+    {
+        private static DiscordSocketClient? _client;
+
+        private static readonly DiscordSocketConfig _socketConfig = new()
+        {
+            GatewayIntents = GatewayIntents.AllUnprivileged | GatewayIntents.GuildMembers | GatewayIntents.GuildPresences | GatewayIntents.MessageContent,
+            AlwaysDownloadUsers = true,
+        };
+
+
+        public static async Task Main(string[] args)
+        {
+
+            using IHost host = Host.CreateDefaultBuilder()
+                .ConfigureServices((_, services) =>
+            services
+            .AddSingleton(x => new DiscordSocketClient(_socketConfig)))
+            .Build();
+
+            using IServiceScope serviceScope = host.Services.CreateScope();
+            IServiceProvider provider = serviceScope.ServiceProvider;
+
+            _client = provider.GetRequiredService<DiscordSocketClient>();
+
+            _client.Log += LogAsync;
+
+            var config = Config.LoadFromFile("config.toml");
+
+            await _client.LoginAsync(TokenType.Bot, config.client.token);
+            await _client.StartAsync();
+            await _client.SetGameAsync(config.client.activity_text, type: config.client.getActivityType());
+            await _client.SetStatusAsync(config.client.getStatus());
+
+            await Task.Delay(Timeout.Infinite);
+
+        }
+
+        private static Task LogAsync(LogMessage log)
+        {
+            Console.WriteLine(log.ToString());
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ Utils/Config.cs
@@ -0,0 +1,47 @@
+﻿using Discord;
+using Tomlyn;
+
+namespace Assistant.Net.Utils
+{
+    public class Config
+    {
+        public Client client { get; set; }
+
+        public static Config LoadFromFile(string filePath)
+        {
+            return Toml.ToModel<Config>(File.ReadAllText(filePath));
+        }
+    }
+    public class Client
+    {
+        public string token { get; set; }
+        public string prefix { get; set; }
+        public string status { get; set; }
+        public string activity_type { get; set; }
+        public string activity_text { get; set; }
+
+        public UserStatus getStatus()
+        {
+            return status switch
+            {
+                "online" => UserStatus.Online,
+                "offline" => UserStatus.Offline,
+                "idle" => UserStatus.Idle,
+                "dnd" => UserStatus.DoNotDisturb,
+                _ => UserStatus.Online
+            };
+        }
+
+        public ActivityType getActivityType()
+        {
+            return activity_type switch
+            {
+                "playing" => ActivityType.Playing,
+                "streaming" => ActivityType.Streaming,
+                "listening" => ActivityType.Listening,
+                "watching" => ActivityType.Watching,
+                _ => ActivityType.Playing
+            };
+        }
+    }
+}
