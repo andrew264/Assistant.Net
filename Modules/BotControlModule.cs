17c21ae42491d707ca9c78f6e3573e9d503160ef
Andrew Santhosh <andrewsanthosh264@gmail.com>
2024-05-13 00:08:28 +0530

    group command: /change {status, avatar, banner} | DMs only

--- Modules/BotControlModule.cs
+++ Modules/BotControlModule.cs
@@ -3,13 +3,15 @@ using Discord.Interactions;
 
 namespace Assistant.Net.Modules;
 
-
+[Group("change", "Change Bot's stuff")]
+[CommandContextType([InteractionContextType.BotDm])]
 public class BotControlModule : InteractionModuleBase<SocketInteractionContext>
 {
     public required InteractionService Commands { get; set; }
+    public required HttpClient _httpClient { get; set; }
 
     [RequireOwner]
-    [SlashCommand("setstatus", "Change Bot's Activity and Presence")]
+    [SlashCommand("status", "Change Bot's Activity and Presence")]
     public async Task SetStatusAsync(
         [Summary(description: "Select Status")] UserStatus status,
         [Summary(description: "Select Activity Type")] ActivityType activityType,
@@ -20,4 +22,53 @@ public class BotControlModule : InteractionModuleBase<SocketInteractionContext>
         await RespondAsync("Status Changed!", ephemeral: true);
     }
 
+    [RequireOwner]
+    [SlashCommand("avatar", "Change Bot's Avatar")]
+    public async Task ChangeAvatarAsync(
+                [Summary(description: "Add an Image")] IAttachment? attachment = null,
+                [Summary(description: "Enter Image URL")] string url = "")
+    {
+
+        if (string.IsNullOrEmpty(url) && attachment == null)
+        {
+            await RespondAsync("Please provide an image URL or attach an image!", ephemeral: true);
+            return;
+        }
+        await DeferAsync(ephemeral: true);
+        try
+        {
+            using var stream = attachment != null ? await _httpClient.GetStreamAsync(attachment.Url) : await _httpClient.GetStreamAsync(url);
+            await Context.Client.CurrentUser.ModifyAsync(x => x.Avatar = new Image(stream));
+            await FollowupAsync("Avatar Changed!");
+        }
+        catch (Exception e)
+        {
+            await FollowupAsync($"Error: {e.Message}");
+        }
+
+    }
+
+    [RequireOwner]
+    [SlashCommand("banner", "Change Bot's Banner")]
+    public async Task ChangeBannerAsync(
+                       [Summary(description: "Add an Image")] IAttachment? attachment = null,
+                       [Summary(description: "Enter Image URL")] string url = "")
+    {
+        if (string.IsNullOrEmpty(url) && attachment == null)
+        {
+            await RespondAsync("Please provide an image URL or attach an image!", ephemeral: true);
+            return;
+        }
+        await DeferAsync(ephemeral: true);
+        try
+        {
+            using var stream = attachment != null ? await _httpClient.GetStreamAsync(attachment.Url) : await _httpClient.GetStreamAsync(url);
+            await Context.Client.CurrentUser.ModifyAsync(x => x.Banner = new Image(stream));
+            await FollowupAsync("Banner Changed!");
+        }
+        catch (Exception e)
+        {
+            await FollowupAsync($"Error: {e.Message}");
+        }
+    }
 }
