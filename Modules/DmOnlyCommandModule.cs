be92f4214815cdc86c82bc052fa94ac1e0b77217
Andrew Santhosh <andrewsanthosh264@gmail.com>
2024-05-19 15:36:01 +0530

    /clear-dm; a command to delete all msgs sent by the bot to your dm

---
+++ Modules/DmOnlyCommandModule.cs
@@ -0,0 +1,36 @@
+﻿using Discord;
+using Discord.Interactions;
+
+namespace Assistant.Net.Modules;
+
+[CommandContextType([InteractionContextType.BotDm])]
+public class DmOnlyCommandModule : InteractionModuleBase<SocketInteractionContext>
+{
+    public required InteractionService Commands { get; set; }
+
+    [SlashCommand("clear-dm", "Delete messages sent by the bot in DM")]
+    public async Task ClearDmMessagesAsync()
+    {
+        if (Context.Channel is not IDMChannel channel)
+        {
+            await RespondAsync("This command can only be used in a DM channel", ephemeral: true);
+            return;
+        }
+        await DeferAsync(ephemeral: true);
+        var messages = (await channel.GetMessagesAsync(100).FlattenAsync()).Where(x => x.Author.Id == Context.Client.CurrentUser.Id);
+
+        if (!messages.TryGetNonEnumeratedCount(out int count))
+            count = messages.Count();
+
+        if (count <= 1)
+        {
+            await ModifyOriginalResponseAsync(x => x.Content = "Couldn't get any deletable messages");
+            return;
+        }
+        foreach (var message in messages)
+            await message.DeleteAsync();
+
+        await ModifyOriginalResponseAsync(x => x.Content = $"Deleted {count - 1} messages");
+    }
+
+}
