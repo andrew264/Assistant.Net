0ae6c5064300e398553bbeadf02b0c71d9799837
Andrew Santhosh <andrewsanthoshc@gmail.com>
2026-02-20 18:44:09 +0530

    remove unused external api stuff

--- src/Assistant.Net/Options/ExternalApiOptions.cs
+++ src/Assistant.Net/Options/ExternalApiOptions.cs
@@ -5,6 +5,4 @@ public sealed class ExternalApiOptions
     public const string SectionName = "ExternalApis";
 
     public string? GeniusToken { get; set; }
-    public string? YoutubeApiKey { get; set; }
-    public string? TenorApiKey { get; set; }
 }
